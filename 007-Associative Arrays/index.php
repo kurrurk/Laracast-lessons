c305477d1172422d61811c8d95a33b879b2005af
Vasily Shatalkin <kuremkarmerurk@gmail.com>
2024-05-07 12:53:44 +0200

    007-Associative Arrays. Urls eingefügt. Hausaufgaben gemacht. Weitere updates für spaß.

--- 007-Associative Arrays/index.php
+++ 007-Associative Arrays/index.php
@@ -6,6 +6,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Laracast 1 - 007-Associative Arrays</title>
 </head>
+<style>
+    body {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        margin: 0;
+        font-family: sans-serif;
+    }
+    em {
+        font-weight: 200;
+    }
+</style>
 <body>
 <?php
 
@@ -14,37 +28,42 @@ $books = [
     [
         'name' => "Project Hail Mary",
         'author' => "Andy Weir",
-        'purchaseUrl' => "https://google.com",
+        'purchaseUrl' => "https://en.wikipedia.org/wiki/Project_Hail_Mary",
         'releaseYear' => 2021
     ],
     [
         'name' => "Artemis",
         'author' => "Andy Weir",
-        'purchaseUrl' => "https://google.com",
+        'purchaseUrl' => "https://en.wikipedia.org/wiki/Artemis_(novel)",
         'releaseYear' => 2017
     ],
     [
         'name' => "Lord of Light",
         'author' => "Roger Zelazny",
-        'purchaseUrl' => "https://google.com",
+        'purchaseUrl' => "https://en.wikipedia.org/wiki/Lord_of_Light",
         'releaseYear' => 1967
     ],
     [
         'name' => "Nine Princes in Amber",
         'author' => "Roger Zelazny",
-        'purchaseUrl' => "https://google.com",
+        'purchaseUrl' => "https://de.wikipedia.org/wiki/Corwin_von_Amber",
         'releaseYear' => 1970
     ]
 
-]
+];
+
+$author = "Roger Zelazny";
 
 ?>
 
+
+<h1>Vorgeschlagene Bücher von <em><?= $author; ?></em>:</h1>
+
 <ul>
     <?php foreach ($books as $book) : ?>
-        <?php if ( $book['author'] === "Roger Zelazny" ) : ?>
+        <?php if ( $book['author'] === $author ) : ?>
             <li>
-                <a href="#">
+                <a href="<?= $book['purchaseUrl']; ?>" target="_blank">
                     <?= $book['name']." (". $book['releaseYear'] .")"; ?> - By <?= $book['author']; ?>
                 </a>
             </li>
