d74f2c4b821183f4b43615fcccf21502844793b3
kurrurk <kuremkarmerurk@gmail.com>
2024-05-10 22:38:12 +0200

    local tailwind-cdn und .idea in .gitignore eingefügt

--- 013-PHP Partials/views/partials/head.php
+++ 013-PHP Partials/views/partials/head.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="/assets/cdn-tailwind.js"></script>
     <title>Laracast - 013-PHP Partials</title>
 </head>
 <body class="h-full">
