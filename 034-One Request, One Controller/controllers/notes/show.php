db79a51edcbd515da8cb8d7a0fdb141521119b82
kurrurk <kuremkarmerurk@jmail.com>
2024-05-31 02:32:59 +0200

    034-One Request, One Controller

---
+++ 034-One Request, One Controller/controllers/notes/show.php
@@ -0,0 +1,23 @@
+<?php
+
+use Core\Database;
+
+$config = require base_path('config.php');
+$db = new Database($config['database'],'kurrurk','L@rAca$7_sql');
+
+$currentUserId = 1;
+
+
+$note = $db->query('select * from notes where id = :id',['id' => $_GET['id']])->findOrFail();
+
+authorize($note['user_id'] === $currentUserId);
+
+view("notes/show.view.php", [
+
+    'heading' => "Notiz",
+    'note' => $note
+
+]);
+
+
+
---
+++ 034-One Request, One Controller/controllers/notes/store.php
@@ -0,0 +1,41 @@
+<?php
+
+use Core\Validator;
+use Core\Database;
+
+$config = require base_path('config.php');
+
+$db = new Database($config['database'],'kurrurk','L@rAca$7_sql');
+
+
+$errors = [];
+
+
+if ( !Validator::string($_POST['body'], 1, 1000) ) {
+
+    $errors['body'] = 'A body of no more then 1000 characters is required';
+
+}
+
+if (!empty($errors)) {
+
+    view("notes/create.view.php", [
+
+        'heading' => "Notiz erstellen",
+        'errors' => $errors
+
+    ]);
+
+}
+
+$db->query('insert into notes(body, user_id) values(:body,:user_id)', [
+
+   'body' => $_POST['body'],
+   'user_id' => 1
+
+]);
+
+header('location: /notes');
+die();
+
+
