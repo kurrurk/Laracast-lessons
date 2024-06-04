674f2fe7b28be9bf085ebdb30f4b962172a4e713
kurrurk <kuremkarmerurk@jmail.com>
2024-06-04 08:47:14 +0200

    036-Updating With PATCH Requests ( nicht fertig )

---
+++ 036-Updating With PATCH Requests/controllers/notes/update.php
@@ -0,0 +1,9 @@
+<?php
+
+// find the corresponding note
+
+// authorize that the current user can edit the note
+
+// validate the form
+
+// if no validation errors, update the record in the note database table
