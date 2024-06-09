00a437f346d56b5410e38fa3c2e70a8facdf8ae7
kurrurk <kuremkarmerurk@gmail.com>
2024-06-10 00:44:00 +0200

    036-Updating With PATCH Requests

--- 036-Updating With PATCH Requests/views/notes/edit.view.php
+++ 036-Updating With PATCH Requests/views/notes/edit.view.php
@@ -9,6 +9,7 @@
 
         <form method="post" action="/note">
             <input type="hidden" name="_method" value="PATCH">
+            <input type="hidden" name="id" value="<?= $note['id']; ?>">
             <div class="space-y-12">
                 <div class="border-b border-gray-900/10 pb-12">
                     <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
