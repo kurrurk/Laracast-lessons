ad8a01fea2d3164c9c0121707d3e852cc069bb2a
VasilyShat <vasily.shatalkin.job@gmail.com>
2025-12-06 17:02:54 +0100

    040-Manage Passwords Like This For The Remainder of Your Career. Removed unnecessary code lines and files in lesson 039.

---
+++ 040-Manage Passwords Like This For The Remainder of Your Career/controllers/registration/store.php
@@ -0,0 +1,52 @@
+<?php
+
+use Core\Validator;
+use Core\App;
+use Core\Database;
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+//validate the form inputs.
+$errors = [];
+if ( !Validator::email($email) ) {
+    $errors['email'] = 'Bitte geben Sie eine gültige E-Mail Adresse an.';
+}
+
+if ( !Validator::string($password, 5, 255) ) {
+    $errors['password'] = 'Falsche Passwortlänge.';
+}
+
+if ( !empty($errors) ) {
+    view('registration/create.view.php', [
+        'errors' => $errors
+    ]);
+}
+
+$db = App::resolve(Database::class);
+// check if the account allready exists
+$user = $db->query('select * from users where email = :email', [
+    'email' => $email
+])->find();
+
+if ($user) {
+    // then someone with that email areadey exist and has account
+    // if yes, redirect to a login page.
+    header('location: /');
+} else {
+    // if not, save one to the database, and then log the user in, and redirect
+    $db->query('insert into users(email, password) values(:email, :password)', [
+        'email' => $email,
+        'password' => password_hash($password, PASSWORD_BCRYPT)
+    ]);
+
+    //mark that the user has logged in
+    //$_SESSION['logged_in'] = true;
+    $_SESSION['user'] = [
+        'email' => $email,
+    ];
+
+    header('location: /');
+    exit();
+}
+
