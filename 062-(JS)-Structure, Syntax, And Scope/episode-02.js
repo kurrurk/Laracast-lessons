b99fabdcf89c638671585eaf9a6d809b72d869c0
VasilyShat <vasily.shatalkin.job@gmail.com>
2025-12-16 15:48:00 +0100

    062-(JS)-Structure, Syntax, And Scope. Changed name of lesson 061

---
+++ 062-(JS)-Structure, Syntax, And Scope/episode-02.js
@@ -0,0 +1,30 @@
+// Types
+
+typeof declareVal;
+
+//undefined
+let declareVal; // undefined
+
+//String
+let doubleQuoted = "double quotes: aren't ";
+let singleQuoted = 'single quotes: "quote this" ';
+
+//Number
+let num = 1;
+let num2 = 0.001;
+
+//Boolean
+let trueVal = true;
+let falseVal = false;
+
+//Synbol
+
+//Object
+let obj = {};
+let nullVal = null;
+
+//Array (not  associative)
+let arr = [];
+
+//function
+let func = function() {}
