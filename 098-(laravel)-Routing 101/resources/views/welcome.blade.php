4e19c545f66ada0b05d15c637832f431608fbc39
VasilyShat <vasily.shatalkin.job@gmail.com>
2026-01-20 18:37:13 +0100

    098-(laravel)-Routing 101

---
+++ 098-(laravel)-Routing 101/resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>098-(laravel)-Routing 101</title>
+</head>
+<body>
+    <h1>Hello, Laravel World!</h1>
+    <div>
+        <a href="/about">About Us</a>
+    </div>
+    <div>
+        <a href="/contact">Contact Us</a>
+    </div>
+</body>
+</html>
