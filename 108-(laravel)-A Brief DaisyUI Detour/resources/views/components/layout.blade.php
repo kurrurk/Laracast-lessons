d2ffb6261871c3b85bfb624cf23431a33dd6640b
VasilyShat <vasily.shatalkin.job@gmail.com>
2026-02-08 11:19:33 +0100

    108-(laravel)-A Brief DaisyUI Detour

--- 108-(laravel)-A Brief DaisyUI Detour/resources/views/components/layout.blade.php
+++ 108-(laravel)-A Brief DaisyUI Detour/resources/views/components/layout.blade.php
@@ -7,7 +7,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>107-(laravel)-Form Request Classes : {{ $title }}</title>
+    <title>108-(laravel)-A Brief DaisyUI Detour : {{ $title }}</title>
     <link href="https://cdn.jsdelivr.net/npm/daisyui@5" rel="stylesheet" type="text/css" />
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
     <link href="https://cdn.jsdelivr.net/npm/daisyui@5/themes.css" rel="stylesheet" type="text/css" />
