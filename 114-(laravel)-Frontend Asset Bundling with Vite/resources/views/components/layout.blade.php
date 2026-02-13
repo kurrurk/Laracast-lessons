2e657a9b41b3909cab6817a6ec01afffca9b59f8
VasilyShat <vasily.shatalkin.job@gmail.com>
2026-02-13 18:23:15 +0100

    114-(laravel)-Frontend Asset Bundling with Vite

---
+++ 114-(laravel)-Frontend Asset Bundling with Vite/resources/views/components/layout.blade.php
@@ -0,0 +1,19 @@
+@props([
+    'title' => 'Laracasts'
+])
+<!DOCTYPE html>
+<html lang="en" data-theme="dracula">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>114-(laravel)-Frontend Asset Bundling with Vite : {{ $title }}</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="">
+    <x-nav />
+    <main class="max-w-3xl mx-auto mt-6">
+        {{ $slot }}
+    </main>
+</body>
+</html>
